c3ee27642f0cb182a2d36e3806bff8853422c181
Matheus Mendes <matheus.mendes@luizalabs.com>
2019-03-15 18:54:26 -0300

    update to env vars

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "835735058634-dg82nfuivu9jlpe65glk8t41nc55dfic.apps.googleusercontent.com"
-const ClientSecret = "c6N1QPhtLnfxkp6DgxFlKmGT"
+const ClientId = os.Getenv("GOOGLE_API_CLIENT_ID")
+const ClientSecret = os.Getenv("GOOGLE_API_CLIENT_SECRET")
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
